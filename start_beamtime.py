96ecb739d624d5d0606a6cd5e79432dc1d6d9d84
chiahaoliu <xf28id1@xf28id1-ws2.cs.nsls2.local>
2015-10-23 18:09:32 -0400

    Fixed conditional statement. It is working now

--- start_beamtime.py
+++ start_beamtime.py
@@ -6,6 +6,12 @@ def start_beamtime():
     import os
     
     HOME_DIR = '/home/xf28id1/xpdUser'
+    
+    W_DIR = '/home/xf28id1/xpdUser/tif_base'
+    D_DIR = '/home/xf28id1/xpdUser/dark_base'
+    R_DIR = '/home/xf28id1/xpdUser/config_base'
+    S_DIR = '/home/xf28id1/xpdUser/script_base'
+
 
     os.chdir(HOME_DIR)
     current_dir = os.getcwd()
@@ -13,12 +19,15 @@ def start_beamtime():
     print(current_dir+'\n')
     
     # fixme check that all of tif_base, dark_base, config_base are empty
-    empty = True
-    if not empty:
+    w_len = len(os.listdir(W_DIR))
+    d_len = len(os.listdir(D_DIR))
+    r_len = len(os.listdir(R_DIR))
+    s_len = len(os.listdir(S_DIR))
+    if not (w_len!=0 and d_len!=0 and r_len!=0 and s_len!=0):
         print('the working directories are not empty.')
         print('if this is really a new beamtime, then please run end_beamtime.py to archive')
         print('the current user-generated data and empty the directories for the new user.')
-        exit()
+        return
     else:
         print('everything is ready to begin.  Please continue with icollection')
 
